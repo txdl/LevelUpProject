5c2c7efa772d63182a3830c0b80455f2a9c4142b
hugobogo <hugosadventures377@gmail.com>
2017-05-09 19:23:59 -0400

    Made Polution Pirate enemy
    We created an enemy ship that randomly moves in directions

---
+++ LevelUpUnityGame/Assets/Scripts/Enemymovement.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var rb: Rigidbody;
+var moveSpeed: float;
+var driftspeed: int;
+var lastInput: Vector3;
+var currentTime: float;
+
+function Start() {
+  rb = GetComponent(Rigidbody);
+}
+function Movement() {
+ var rando: Vector3 = new Vector3(Random.Range(-1.0,1.0), 0, Random.Range(-1.0,1.0)); 
+ rb.AddForce (rando * moveSpeed,ForceMode.Impulse);
+ Debug.Log(rando);
+}
+function Update () {
+	if(Time.time > currentTime) { 
+		Movement () ; 
+		currentTime+=7;
+	}
+}
