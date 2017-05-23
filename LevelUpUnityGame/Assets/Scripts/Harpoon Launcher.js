b81dfdc30628739902cbc140c448b3a68f246f1e
Richard Pringle <rpring9@gmail.com>
2017-05-23 09:42:07 -0400

    Convert hard tabs to two space tabs.

--- LevelUpUnityGame/Assets/Scripts/Harpoon Launcher.js
+++ LevelUpUnityGame/Assets/Scripts/Harpoon Launcher.js
@@ -9,24 +9,24 @@ var line: LineRenderer;
 
 
 function Start() {
-	player = GameObject.Find("Playerboat");
+  player = GameObject.Find("Playerboat");
 }
 
 function Update() {
-	howfar = Vector3.Distance(player.transform.position, gameObject.transform.position);
-	
+  howfar = Vector3.Distance(player.transform.position, gameObject.transform.position);
+  
   if (howfar <= firerdist && harpoonamt > 0){
-		Fire();
-	}
+    Fire();
+  }
 
-	line.SetPosition(0,transform.position);
-	line.SetPosition(1,player.transform.position);
+  line.SetPosition(0,transform.position);
+  line.SetPosition(1,player.transform.position);
 }
 
 
 function Fire() {
-	harpoonamt--;
-	Debug.Log('fire');
-	sj.connectedBody = player.GetComponent.<Rigidbody>();
-	line.enabled = true;
+  harpoonamt--;
+  Debug.Log('fire');
+  sj.connectedBody = player.GetComponent.<Rigidbody>();
+  line.enabled = true;
 }
