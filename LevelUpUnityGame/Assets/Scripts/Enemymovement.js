c9f17482afe22d9965a5dbf3c68f9ac2c1b0fe4d
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2017-05-23 19:21:43 -0400

    Yahar, pirates apleanty.

--- LevelUpUnityGame/Assets/Scripts/Enemymovement.js
+++ LevelUpUnityGame/Assets/Scripts/Enemymovement.js
@@ -2,6 +2,8 @@
 
 var rb: Rigidbody;
 var moveSpeed: float;
+var isHarpooned: boolean = false;
+var isChasing: boolean = false;
 var driftspeed: int;
 var lastInput: Vector3;
 var currentTime: float;
@@ -10,19 +12,36 @@ var playerPosition: Vector3;
 var playerboat: GameObject;
 var enemyPosition: Vector3;
 var attackVector: Vector3;
+var sj: SpringJoint;
 
 function Start() {
   rb = GetComponent(Rigidbody);
+  sj = GetComponent(SpringJoint);
   playerboat = GameObject.Find("Playerboat");
-  attackRadius = 15;
+  // attackRadius = 15;
 }
 
 function Update () {
 	enemyPosition = this.gameObject.transform.position;
 	playerPosition = playerboat.transform.position;
 	attackVector = playerPosition - enemyPosition;
+	
 	if (attackVector.magnitude < attackRadius) {
-		Debug.Log('attack');
-		rb.AddForce(attackVector * moveSpeed, ForceMode.Impulse);
+		isChasing = true;
 	}
+
+	if (isChasing && !isHarpooned) {
+		attackVector.Normalize();
+		rb.AddForce(attackVector * moveSpeed, ForceMode.Acceleration);
+
+		sj.anchor = enemyPosition;
+	}
+}
+
+function startChasing() {
+
+}
+
+function harpoonPlayer() {
+	isHarpooned = true;
 }
