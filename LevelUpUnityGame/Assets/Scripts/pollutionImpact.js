e322576d5f388d23381748c981b1cff259023098
Richard Pringle <rpring9@gmail.com>
2017-05-23 09:51:01 -0400

    Cleanup.

--- LevelUpUnityGame/Assets/Scripts/pollutionImpact.js
+++ LevelUpUnityGame/Assets/Scripts/pollutionImpact.js
@@ -1,4 +1,5 @@
 ﻿#pragma strict
+
 var otherScript: playerscoreboard;
 var score: float;
 var durationOfStay: float;
@@ -6,38 +7,38 @@ var ps: ParticleSystem;
 
 
 function OnTriggerEnter(other : Collider) {
-	if (other.gameObject.layer == 9) {
-		durationOfStay = 0;
-				Debug.Log("hit");
-	}
+  if (other.gameObject.layer == 9) {
+    durationOfStay = 0;
+    Debug.Log("hit");
+  }
 }
 
 function OnTriggerStay(other: Collider) {
-	if (other.gameObject.layer == 9) {
-		
-		// ps.colorOverLifetime.color.a = 0.1;
-		durationOfStay++ ;
-		var start = ps.main.startColor.colorMax;
-		var end = ps.main.startColor.colorMin;
-		var transparency = 1- (durationOfStay*0.25/60);
-		ps.colorOverLifetime.color = new ParticleSystem.MinMaxGradient(new Color(start.r, start.g, start.b, transparency), new Color(end.r, end.g, end.b, transparency));
+  if (other.gameObject.layer == 9) {
+    // ps.colorOverLifetime.color.a = 0.1;
+    durationOfStay++ ;
 
- 		//add despawn Pollution pool after 180 frames of durationOfStay
- 		//reinitiate Pollution pool if under 180 frames of durationOfStay
+    var start = ps.main.startColor.colorMax;
+    var end = ps.main.startColor.colorMin;
+    var transparency = 1 - (durationOfStay * 0.25 / 60);
 
+    var colorStart = new Color(start.r, start.g, start.b, transparency);
+    var colorEnd = new Color(end.r, end.g, end.b, transparency);
+    ps.colorOverLifetime.color = new ParticleSystem.MinMaxGradient(colorStart, colorEnd);
 
-		
-	}
+    //add despawn Pollution pool after 180 frames of durationOfStay
+    //reinitiate Pollution pool if under 180 frames of durationOfStay   
+  }
 }
 
 // function OnTriggerExit(other: Collider) {
-// 	if ( ***
-// 		//despawn pollution
-// 		// otherScript.increasescore(score);
-// 				Debug.Log("clean");
-// 		)
+//   if ( ***
+//     //despawn pollution
+//     // otherScript.increasescore(score);
+//         Debug.Log("clean");
+//     )
 // }
 
 function Update () {
-	
+  
 }
