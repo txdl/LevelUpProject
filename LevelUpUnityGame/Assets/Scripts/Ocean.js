c7de397740c025cebb6d21e6b81fc06a4254a94e
MartochAZ <jasontruesdell@gmail.com>
2017-05-23 19:17:02 -0400

    Merge remote-tracking branch 'refs/remotes/origin/master' into MartochAZ2
    # Conflicts:
    #	LevelUpUnityGame/Assets/Scripts/pollutionImpact.js

---
+++ LevelUpUnityGame/Assets/Scripts/Ocean.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+var target: Transform;
+
+function LateUpdate() {
+	transform.position = new Vector3 (target.position.x, 0, target.position.z);
+}
---
+++ LevelUpUnityGame/Assets/Scripts/WorldGeneration.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var rock: GameObject;
+var rockAmt = 22;
+var boat: GameObject;
+var pool: GameObject;
+var pirate: GameObject;
+var velocity: float;
+var acceleration: float;
+var vector: Vector3;
+var boatPosition: Vector3;
+
+
+function OnTriggerExit(coll: Collider) {
+	coll.gameObject.transform.position = boat.transform.forward * 200;
+}
